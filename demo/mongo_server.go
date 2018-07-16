fe07f8fbaf10f7897f32fda8eaae961186c500bd
agussa <kang_ib@yahoo.com>
2018-07-16 19:30:03 +0700

    Add files via upload

---
+++ demo/mongo_server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	imap "github.com/jordwest/imap-server"
+	"github.com/jordwest/imap-server/mailstore"
+)
+
+func main() {
+	store := mailstore.NewMongoDBMailstore()
+	s := imap.NewServer(store)
+	s.Transcript = os.Stdout
+	s.Addr = ":10143"
+
+	err := s.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Error creating test connection: %s\n", err)
+	}
+}
